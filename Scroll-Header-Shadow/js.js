b4b12037962d8ff4b7aa81b9768e084a0e239612
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-04-04 23:10:13 +0300

    Add files via upload

---
+++ Scroll-Header-Shadow/js.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("page-header");
+const intercept = document.createElement("div");
+
+intercept.setAttribute("data-observer-intercept", "");
+header.before(intercept);
+
+const observer = new IntersectionObserver(([entry]) => {
+  header.classList.toggle("active", !entry.isIntersecting);
+});
+
+observer.observe(intercept);

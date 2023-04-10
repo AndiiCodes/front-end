5309ba40489ef41264179f95524a3efec642f1b7
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-04-10 15:06:10 +0300

    Create script.js

---
+++ Elastic-Accordion/script.js
@@ -0,0 +1,27 @@
+// script.js 
+const items = document.querySelectorAll('.item')
+
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return
+    it.clicked = false
+  })
+  gsap.to(items, {
+    width: item.clicked ? '15vw' : '8vw',
+    duration: 2,
+    ease: 'elastic(1, .6)'
+  })
+  
+  item.clicked = !item.clicked
+  gsap.to(item, {
+    width: item.clicked ? '42vw' : '15vw',
+    duration: 2.5,
+    ease: 'elastic(1, .3)'
+  })
+}
+
+items.forEach((item, i) => {
+  item.clicked = false
+  item.addEventListener('click', () => expand(item, i))
+})
+

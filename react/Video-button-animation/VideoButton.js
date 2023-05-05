d7e582047eb2db99eb79ef8f8838547c595f66c4
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-05-06 01:53:23 +0300

    Update VideoButton.js

--- react/Video-button-animation/VideoButton.js
+++ react/Video-button-animation/VideoButton.js
@@ -6,7 +6,7 @@ const VideoButton = () => {
   <input type="checkbox" />
   <div className="video">
     <video loop={true} muted={true} autoPlay={true}> 
-      <source src="video.mp4" type="video/mp4"  />
+      <source src="https://www.robmillsarchitects.com/files/land/city/RMA_Web_land_city_1.mp4" type="video/mp4"  />
     </video>
   </div>
   <div className="text">

02a6354cc7322dad20eb9047aa6e4145cb21d634
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-05-08 22:15:30 +0300

    Create card.js

---
+++ react/border-card-animation/card.js
@@ -0,0 +1,17 @@
+// Card.js 
+const Card = () => {
+  return (
+   <>
+    <div className="card">
+  <div className="card-content">
+    <h3>Valuable Info</h3>
+    <h1>The worst thing about coding is the dementors</h1>
+    <p>I can't stand when I have spent the last 47 minutes adjusting the rgb on my gradient only to have a dementor appear and suck my soul out.</p>
+  </div>
+</div>
+   </>
+    
+  );
+};
+
+export default Card;

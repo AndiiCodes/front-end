9efeee9ca968531b8a8315ef0ee9a9d1822d24b1
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-05-13 15:25:07 +0300

    Create SwitchButton.js

---
+++ react/theme-toggle-switch/SwitchButton.js
@@ -0,0 +1,14 @@
+// SwitchButton.js
+import React from 'react'
+
+export default function SwitchButton() {
+  return (
+    <div>
+    <div className="container">
+  <input id="checkbox" name="checkbox" type="checkbox" />
+  <label className="label" htmlFor="checkbox"></label>
+</div>
+
+    </div>
+  )
+}

2ae9f174dc42adc76c17abf232642afdd9400c12
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-05-23 01:00:35 +0300

    Create Card.js

---
+++ react/hover-info-card/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function CardContainer() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>North</h2>
+        <i className="fas fa-arrow-right"></i>
+        <p>a lonely trip.</p>
+        <div className="pic"></div>
+        <div className="social">
+          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-instagram"></i>
+          <i className="fab fa-github"></i>
+        </div>
+        <button></button>
+      </div>
+
+      
+    </div>
+  );
+}

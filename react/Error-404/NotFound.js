168054dd699754770c02bd3224c9daf53e3f7650
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-05-27 01:37:59 +0300

    Create NotFound.js

---
+++ react/Error-404/NotFound.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <div id="oopss">
+  <div id="error-text">
+    <img
+      src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+      alt={404}
+    />
+    <span>Error 404</span>
+    <p className="p-a">Page not found! Looks like the URL went on a vacation without leaving a forwarding address. Let's hope it's enjoying some sunny beaches and will be back soon!</p>
+    <p className="p-b">You will be redirected to the homepage.</p>
+  </div>
+</div>
+
+    </div>
+  )
+}

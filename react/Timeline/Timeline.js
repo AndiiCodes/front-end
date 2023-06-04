60e79972d40e0ed80d45a90cd3d0ff0c4d4eea6d
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-06-04 16:14:48 +0300

    Create Timeline.js

---
+++ react/Timeline/Timeline.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+export default function Timeline() {
+  return (
+    <div>
+      <div className="entries">
+  <div className="entry">
+    <div className="title">2016</div>
+    <div className="body">
+      <p>
+        Neque sunt voluptatibus repellat pariatur ut enim. Eveniet rerum
+        suscipit eveniet amet dignissimos. Doloremque et distinctio quod
+        molestiae ut.
+      </p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title">2017</div>
+    <div className="body">
+      <p>Quo nobis cumque dolor iure voluptatem voluptatem alias soluta.</p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title big">2018</div>
+    <div className="body">
+      <p>
+        Rerum sit libero possimus amet excepturi. Exercitationem enim dolores
+        sunt praesentium dolorum praesentium.
+      </p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title">2019</div>
+    <div className="body">
+      <p>
+        Voluptatibus veniam ea reprehenderit atque. Reiciendis non laborum
+        adipisci ipsa pariatur omnis. Sed ipsam repudiandae velit. Omnis libero
+        nostrum aperiam nemo dolor ea eos eius. Esse a non itaque quidem.
+      </p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title">2020</div>
+    <div className="body">
+      <p>
+        VAdipisci totam omnis cum et suscipit excepturi et excepturi. Inventore
+        sequi sit ut aliquid. Modi aut dolores dignissimos.
+      </p>
+      <p>
+        Delectus facere officia consequuntur molestias deserunt illo. Placeat
+        laudantium beatae natus excepturi ab nihil voluptates.
+      </p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title big">2021</div>
+    <div className="body">
+      <p>
+        Impedit dolorem commodi explicabo fugit aut alias voluptatem. Magnam
+        earum rerum quae dicta quibusdam aliquam ut.
+      </p>
+    </div>
+  </div>
+  <div className="entry">
+    <div className="title">2022</div>
+    <div className="body">
+      <p>Qui facere eos aut suscipit doloremque quos...</p>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+  )
+}

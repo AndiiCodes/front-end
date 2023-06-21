502618e1df893b579a3142fcfab38dccd1aafb66
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-06-21 23:30:18 +0300

    Create script.js

---
+++ dropdown-list/script.js
@@ -0,0 +1,21 @@
+const selectBtn = document.querySelector(".select-btn"),
+      items = document.querySelectorAll(".item");
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+            if(checked && checked.length > 0){
+                btnText.innerText = `${checked.length} Selected`;
+            }else{
+                btnText.innerText = "Select Language";
+            }
+    });
+})

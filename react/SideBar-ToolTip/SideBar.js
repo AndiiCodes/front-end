c85d91a109ac2f32b87d9f4494c8e26bb459a782
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-01 21:57:01 +0300

    Create SideBar.js

---
+++ react/SideBar-ToolTip/SideBar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div>
+    <>
+  <ul>
+    <li>
+      <a href="#">
+        <i className="ai-home" />
+        <span>Dashboard</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-image" />
+        <span>Images</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-file" />
+        <span>Files</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-game-controller" />
+        <span>Games</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-book-open" />
+        <span>Books</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-bell" />
+        <span>Notifications</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-gear" />
+        <span>Settings</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i className="ai-person" />
+        <span>Profile</span>
+      </a>
+    </li>
+  </ul>
+</>
+
+    </div>
+  )
+}

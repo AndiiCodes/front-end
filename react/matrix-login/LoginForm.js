a1624a81f5e7a6000d54bd9e8abf9fb972d2e879
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-10 01:43:46 +0300

    Create LoginForm.js

---
+++ react/matrix-login/LoginForm.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <>
+  <meta charSet="UTF-8" />
+  <title>CodePen - Animated Login Form using Html &amp; CSS Only</title>
+  <link rel="stylesheet" href="./style.css" />
+  
+  <section>
+    {" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span /> <span /> <span /> <span /> <span />{" "}
+    <span /> <span /> <span /> <span />
+    <div className="signin">
+      <div className="content">
+        <h2>Sign In</h2>
+        <div className="form">
+          <div className="inputBox">
+            <input type="text" required="" /> <i>Username</i>
+          </div>
+          <div className="inputBox">
+            <input type="password" required="" /> <i>Password</i>
+          </div>
+          <div className="links">
+            {" "}
+            <a href="#">Forgot Password</a> <a href="#">Signup</a>
+          </div>
+          <div className="inputBox">
+            <input type="submit" defaultValue="Login" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>{" "}
+  
+</>
+
+  )
+}

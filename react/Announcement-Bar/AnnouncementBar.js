39210efafdef909d0d532a9c7676eea576e009a0
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-16 02:36:33 +0300

    Create AnnouncementBar.js

---
+++ react/Announcement-Bar/AnnouncementBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AnnouncementBar() {
+  return (
+    <div>
+    <button type="button">
+  <span>Telegram @AndiiCodes</span>
+  <span>Telegram @AndiiCodes</span>
+</button>
+
+    </div>
+  )
+}

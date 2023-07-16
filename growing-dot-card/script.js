f8cca86cf4ceb39157f66dcfb87797df69be82b9
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-17 01:52:11 +0300

    Create script.js

---
+++ growing-dot-card/script.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 100; i++) {
+const gridEffectTile = document.createElement("a");
+gridEffectTile.setAttribute("class", "card__grid-effect-tile");
+gridEffectTile.setAttribute("href", "#");
+document.querySelector(".card__grid-effect").appendChild(gridEffectTile);
+}
+            

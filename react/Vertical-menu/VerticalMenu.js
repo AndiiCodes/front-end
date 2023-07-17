a497581463c4d3d1c1d84c74214cb071a67e21dc
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-17 22:51:02 +0300

    Create VerticalMenu.js

---
+++ react/Vertical-menu/VerticalMenu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function VerticalMenu() {
+  return (
+    <>
+     <ul className="side-menu">
+  <li>
+    <a href="#">
+      <span className="fa fa-code" />
+      Superguide
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa fa-cog" />
+      Installation and Dependencies
+
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa fa-font" />
+      Typography
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa fa-caret-square-o-right" />
+      Buttons
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa fa-check-square" />
+      Forms
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa  fa-square" />
+      Cards
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span className="fa fa-bars" />
+      Menu
+    </a>
+  </li>
+</ul>
+
+
+    </>
+
+  )
+}

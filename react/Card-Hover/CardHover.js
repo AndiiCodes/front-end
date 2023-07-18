af06414b63c3493dd4f28290217542d1fd4db6a3
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-18 22:46:24 +0300

    Create CardHover.js

---
+++ react/Card-Hover/CardHover.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function CardHover() {
+  return (
+    <>
+     <div className="card">
+  <div className="card-details">
+    <p className="text-title">Card title</p>
+    <p className="text-body">Lorem, ipsum...</p>
+  </div>
+  <button className="card-button">More info</button>
+</div>
+    </>
+  )
+}

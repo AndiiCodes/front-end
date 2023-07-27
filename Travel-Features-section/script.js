fc6e84fe4601f21b4159e8709c250645c8dc30af
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-07-28 00:37:36 +0300

    Create script.js

---
+++ Travel-Features-section/script.js
@@ -0,0 +1,14 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      }
+    }
+  });
+  

2b7be0499e3d9c2ee34a08395ddea982e0fe6496
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-08-03 03:23:27 +0300

    Create Loader.js

---
+++ react/embed-videos/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div>
+    <div className="dot-spinner">
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+    <div className="dot-spinner__dot"></div>
+</div>
+    </div>
+  )
+}

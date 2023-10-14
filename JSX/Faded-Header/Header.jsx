f5f07112c4496abbfd5bd052f468113877bca1e9
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-10-14 16:51:28 +0300

    Create Header.jsx

---
+++ JSX/Faded-Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function App() {
+  return (
+          <div>
+          <nav>
+        <div class="menu">
+          <p>Cool Site</p>
+          <button>Sign up</button>
+        </div>
+        <div class="backdrop"></div>
+      </nav>
+
+      <main>
+        <h1>The Coolest Header Ever</h1>
+        <p>This is literally one of the coolest headers.</p>
+      </main>
+          </div>
+  )
+}
